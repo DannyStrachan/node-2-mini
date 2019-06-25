b866e01eaf8357be13eee01b9b65e5b15354c87d
Danny Strachan <dannystrachan@Dannys-MacBook-Pro.local>
2019-06-25 09:08:59 -0600

    completed project

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const bc = require("./controllers/books_controller.js");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname + "/../build"));
+
+app.get("/api/books", bc.read);
+
+app.post("/api/books", bc.create);
+
+app.put("/api/books/:id", bc.update);
+
+app.delete("/api/books/:id", bc.delete);
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server is live on port ${port}`);
+});
